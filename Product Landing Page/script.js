7a9ec9409a18325a027ee2da933e0af7b2ca36cb
Ansari Affan <92035762+AffanAnsari786@users.noreply.github.com>
2023-07-29 23:10:33 +0530

    Add files via upload

---
+++ Product Landing Page/script.js
@@ -0,0 +1,10 @@
+function imgSlider(imgSrc){
+    const imgSlider = document.querySelector('.main-product');
+    imgSlider.src = imgSrc;
+   
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
